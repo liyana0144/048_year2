8c39ce71ab625365c53b750a428f8c0d8d6d6219
liyana0144 <78543927+liyana0144@users.noreply.github.com>
2021-11-24 14:42:17 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,55 +1,103 @@
 ﻿using System;
 
-namespace ConsoleApp8_qz2
+namespace HW3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input rose : ");
-            int Roseid = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input sunflower : ");
-            int SunFlowerid = int.Parse(Console.ReadLine());
+            Queue CPUQueue = new Queue();
+            char instruction;
+            char data;
+            int count = 0;
+            int Round = 0;
+            while (true)
+            {
+                instruction = char.Parse(Console.ReadLine());
 
+                if (instruction == '?')
+                {
+                    break;
+                }
+                data = char.Parse(Console.ReadLine());
+                Node CPU = new Node(instruction, data);
+                CPUQueue.Push(CPU);
+
+            }
+            Node instQueue;
+            while (true)
+            {
+                instQueue = CPUQueue.Pop();
+                count++;
+                if (instQueue == null)
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("CPU cycles needed:" + count);
         }
     }
-    class ROSE
+    class Node
     {
-        public int ID;
-        public string NameFlower;
-        public string Discription;
-        public int Amount;
-        public string Hight;
-        public string Cercumference;
-
-        public ROSE(int id, string nameflower, string discription, int amount, string hight, string cercumference)
+        public char Instruction;
+        public char Data;
+        public Node Next;
+        public Node(char instructionValue, char dataValue)
         {
-            this.ID = id;
-            this.NameFlower = nameflower;
-            this.Discription = discription;
-            this.Amount = amount;
-            this.Hight = hight;
-            this.Cercumference = cercumference;
+            Instruction = instructionValue;
+            Data = dataValue;
+        }
+        public override string ToString()
+        {
+            return String.Format("({0}, {1})", Instruction, Data);
         }
 
     }
-    class SUNFLOWER
+    class Queue
     {
-        public int ID;
-        public string NameFlower;
-        public string Discription;
-        public int Amount;
-        public string Hight;
-        public string Cercumference;
-
-        public SUNFLOWER(int id, string nameflower, string discription, int amount, string hight, string cercumference)
+        private Node Root;
+        public void Push(Node node)
         {
-            this.ID = id;
-            this.NameFlower = nameflower;
-            this.Discription = discription;
-            this.Amount = amount;
-            this.Hight = hight;
-            this.Cercumference = cercumference;
+            if (Root == null)
+            {
+                Root = node;
+            }
+            else
+            {
+                Node ptr = Root;
+                while (ptr.Next != null)
+                {
+                    ptr = ptr.Next;
+                }
+                ptr.Next = node;
+            }
+        }
+        public Node Pop()
+        {
+            if (Root == null)
+            {
+                return null;
+            }
+            Node node = Root;
+            Root = Root.Next;
+            node.Next = null;
+            return node;
+        }
+        public Node Get(int index)
+        {
+            Node node = Root;
+            while (index > 0)
+            {
+                if (node == null)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                node = node.Next;
+                index--;
+            }
+            return node;
         }
     }
 }
